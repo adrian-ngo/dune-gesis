62eb902a8750d4a325dfb85c0c86ffa436759885
Adrian Ngo <ngo@eclipso.de>
2014-09-05 13:25:53 +0200

    Add new bash script to handle release and debug builds and move build result to the new working directories.

---
+++ src/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ "$2" == "dbg" ];
+then
+    echo "build $1 debug version"
+    build_flags="-O0 -g -Wall"
+    build_dir="../dbg"
+else
+    echo "build $1 release version"
+    build_flags="-O3 -funroll-loops -DNDEBUG"
+    build_dir="../bin"
+fi
+#
+make CXXFLAGS="$build_flags" $1
+if [ -f "$1" ];
+then
+    mv -v $1 $build_dir
+else
+    echo "File not found: $1"
+fi
