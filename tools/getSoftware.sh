5215016b990ce9969863acc743fe72bea38583a6
Adrian Ngo <ngo@eclipso.de>
2014-11-25 18:26:54 +0100

    boost 1.47

--- tools/getSoftware.sh
+++ tools/getSoftware.sh
@@ -3,6 +3,6 @@ wget http://www.fftw.org/fftw-3.3.4.tar.gz
 echo "Get HDF5..."
 wget http://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.13.tar.gz
 echo "Get BOOST..."
-wget http://sourceforge.net/projects/boost/files/boost/1.57.0/boost_1_57_0.tar.gz/download
+wget http://sourceforge.net/projects/boost/files/boost/1.47.0/boost_1_47_0.tar.gz/download
 echo "Get UG..."
 wget http://conan.iwr.uni-heidelberg.de/download/ug-3.11.0.tar.gz
