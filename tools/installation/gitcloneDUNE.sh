b73085ac6cf097b4e0564625578e660864bbad40
Adrian Ngo <ngo@eclipso.de>
2014-11-26 20:17:31 +0100

    correct branch

--- tools/installation/gitcloneDUNE.sh
+++ tools/installation/gitcloneDUNE.sh
@@ -10,7 +10,7 @@ git clone -b releases/2.3 http://git.dune-project.org/repositories/dune-grid
 git clone -b releases/2.3 http://git.dune-project.org/repositories/dune-istl
 git clone -b releases/2.3 http://git.dune-project.org/repositories/dune-localfunctions
 git clone -b releases/2.3 http://git.dune-project.org/repositories/dune-typetree
-git clone -b releases/2.3 http://git.dune-project.org/repositories/dune-pdelab
+git clone -b releases/2.0 http://git.dune-project.org/repositories/dune-pdelab
 #
 #
 # Download the DUNE module for ALUGrid (optional for DUNE-GESIS) via git:
